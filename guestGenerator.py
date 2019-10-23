c92ac27c607305687fcddc41c5dfb641d840e314
Justyna Gorczyca <just.gorcz@gmail.com>
2019-10-24 00:34:44 +0200

    generator ready

--- guestGenerator.py
+++ guestGenerator.py
@@ -1,18 +1,28 @@
 from lxml import etree
 import random
 
+guestsNumber = 10
+
 def generateGuests(guestsNumber, document):
+    fivesLeft = guestsNumber/2
     counter = 0
     for i in range(guestsNumber):
         for j in range (i+1, guestsNumber):
             guestsPair = "[g" + str(i) + "][g" + str(j) + "]"
-            guestsRelation = str(random.randint(0,5))
-            #print(guestsPair + " = " + guestsRelation)
-            etree.SubElement(document, "pair", name = guestsPair).text = guestsRelation
+
+            guestsRelation = random.randint(0,5)
+            if guestsRelation == 5:
+                if fivesLeft <= 0:
+                    while guestsRelation == 5:
+                        guestsRelation = random.randint(0,5)
+                else:
+                    fivesLeft -= 1
+
+            etree.SubElement(document, "pair", name = guestsPair).text = str(guestsRelation)
             counter+=1
 
 def main():
-    guestsNumber = 100
+
     root = etree.Element("root")
     etree.SubElement(root, "guestsNumber", name = str(guestsNumber))
     document = etree.SubElement(root, "guestsList")
--- guestsManager.py
+++ guestsManager.py
@@ -1,18 +1,16 @@
 from lxml import etree
 
-def main():
-    file = open("generatedData.xml")
-    tree = file.read()
-    root = etree.fromstring(tree)
+guestsDictionary = dict()
+guestsList = []
 
-    guestsDictionary = dict()
+def createGuestsDictionary(root):
     for el in root.getchildren():
         for guest in el.getchildren():
             pair = guest.attrib['name']
             value = guest.text
             guestsDictionary[pair] = value
 
-    guestsList = []
+def createGuestsList(root):
     firstPair = root[1][1].attrib['name']
     firstGuest = firstPair[1:-5]
     guestsNumber = int(root[0].attrib['name'])
@@ -22,9 +20,67 @@ def main():
         guest = root[1][i].attrib['name'][5:-1]
         guestsList.append(guest)
 
-    for el in guestsList:
-        print(el)
-    print(len(guestsList))
+def setGuestsInOrder():
+    resultFile = open("guestsInOrder.txt", "w")
+    for guest in guestsList:
+        guestsInOrder = guest
+        leftGuests = guestsList.copy()
+        leftGuests.remove(guest)
+        doStaff(guestsInOrder, leftGuests, resultFile, 0)
+
+def findBestFriend(person, leftGuests):
+    bestFriend = ""
+    maxFriendshipLevel = 0
+
+    personNumber = person[1:]
+    #print("\nszukam kolegi dla: " + personNumber)
+
+    for guest in leftGuests:
+        if person == guest:
+            continue
+
+        if maxFriendshipLevel == 5:
+           break
+
+        guestNumber = guest[1:]
+
+        if int(personNumber) > int(guestNumber):
+            pair = "[" + guest + "][" + person + "]"
+        else:
+            pair = "[" + person + "][" + guest + "]"
+
+        friendshipLevel = int(guestsDictionary[pair])
+        #print(pair + " = " + str(friendshipLevel))
+        if friendshipLevel > maxFriendshipLevel:
+            maxFriendshipLevel = friendshipLevel
+            bestFriend = guest
+
+    #print("<3 = " + bestFriend)
+    return bestFriend, int(maxFriendshipLevel)
+
+def doStaff(guestsInOrder, leftGuests, resultFile, sumOfFrienship):
+    if len(leftGuests) > 0:
+        people = guestsInOrder.split('-')
+        person = people[len(people)-1]
+        bestFriend, friendshipLevel = findBestFriend(person, leftGuests)
+        if int(friendshipLevel) > 0:
+            leftGuestsWithoutFriend = leftGuests.copy()
+            leftGuestsWithoutFriend.remove(bestFriend)
+            doStaff(guestsInOrder + "-(" + str(friendshipLevel) + ")-" + bestFriend, leftGuestsWithoutFriend, resultFile, sumOfFrienship + friendshipLevel)
+    else:
+        print(guestsInOrder + "  ->  " + str(sumOfFrienship))
+        resultFile.write(guestsInOrder + "  ->  " + str(sumOfFrienship) + "\n")
+
+def main():
+    file = open("generatedData.xml")
+    tree = file.read()
+    root = etree.fromstring(tree)
+
+    createGuestsDictionary(root)
+    createGuestsList(root)
+    setGuestsInOrder()
+
+    file.close()
 
 if __name__ == "__main__":
     main()
